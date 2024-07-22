7a425e9794b7e605d1232887ac2f0c37812d4da1
Zen.Liu <lcz20@163.com>
2024-07-22 14:26:34 +0800

    fix SSE

--- htt/sse.go
+++ htt/sse.go
@@ -2,21 +2,41 @@ package htt
 
 import (
 	"bytes"
-	"github.com/ZenLiuCN/fn"
+	"context"
+	"github.com/ZenLiuCN/gofra/conf"
+	"io"
+	"log"
+	"net"
 	"net/http"
 	"strconv"
 	"strings"
 )
 
+// SSE component to send Server-Send-Event to http client.
+//
+// A SSE can only use once. [SSE.Await] should only call inside [http.Handler]. it will block until all events are done.
+// All send methods may return [io.EOF] if the SSE already shutdown.
 type SSE interface {
-	Send(data string) error
-	Ping() error
-	SendEvent(event, data string) error
-	SendEventID(id, event, data string) error
-	Retry(mills int) error
-	Raw() http.ResponseWriter
+	Close() error                             //close the SSE, returns [io.EOF] if already closed.
+	Send(data string) error                   //alias of SendEventId.
+	Ping() error                              //send a ping comment, returns [io.EOF] if already closed.
+	SendEvent(event, data string) error       //alias of SendEventId.
+	SendEventID(id, event, data string) error //send event to SSE client, empty string will not write to stream, returns [io.EOF] if already closed.
+	Retry(mills int) error                    // send retry to SSE client, returns [io.EOF] if already closed.
+	Raw() http.ResponseWriter                 // the internal response writer.
+	Await(ctx context.Context) error          // Await with optional context inside [http.Handler] , returns [io.EOF] if SSE already closed or the client closed connection.
 }
-type sse [1]http.ResponseWriter
+type sse struct {
+	ch chan *msg
+	http.ResponseWriter
+}
+type msg struct {
+	ping  bool
+	retry uint32
+	event string
+	id    string
+	data  string
+}
 
 var (
 	bLF    = []byte("\n")
@@ -27,77 +47,166 @@ var (
 	bPing  = []byte(": ping\n\n")
 )
 
-func (s sse) Raw() http.ResponseWriter {
-	return s[0]
-}
-func (s sse) Ping() (err error) {
-	_, err = s[0].Write(bPing)
-	return
-}
-func (s sse) SendEventID(id, event, data string) error {
-	buf := fn.GetBuffer()
-	defer func() {
-		fn.PutBuffer(buf)
-	}()
-	if id != "" {
-		buf.Write(bId)
-		buf.WriteString(id)
-		buf.Write(bLF)
+func (s *sse) Close() error {
+	if s.ch == nil {
+		return io.EOF
 	}
-	if event != "" {
-		buf.Write(bEvent)
-		buf.WriteString(event)
-		buf.Write(bLF)
-	}
-	if data != "" {
-		i := strings.IndexByte(data, '\n')
-		for i >= 0 {
-			buf.Write(bData)
-			buf.WriteString(data[:i])
-			buf.Write(bLF)
-			data = data[i+1:]
-			i = strings.IndexByte(data, '\n')
-		}
-		if len(data) > 0 {
-			buf.Write(bData)
-			buf.WriteString(data)
-			buf.Write(bLF)
-		}
-	}
-	buf.Write(bLF)
-	return s.send(buf)
+	close(s.ch)
+	s.ch = nil
+	return nil
 }
-func (s sse) Retry(mills int) error {
-	buf := fn.GetBuffer()
-	defer func() {
-		fn.PutBuffer(buf)
-	}()
-	buf.Write(bRetry)
-	buf.WriteString(strconv.Itoa(mills))
-	buf.Write(bLF)
-	return s.send(buf)
+func (s *sse) Raw() http.ResponseWriter {
+	return s.ResponseWriter
+}
+func (s *sse) Ping() (err error) {
+	return s.send(&msg{ping: true})
+}
+func (s *sse) SendEventID(id, event, data string) error {
+	return s.send(&msg{id: id, event: event, data: data})
+}
+func (s *sse) Retry(mills int) error {
+	return s.send(&msg{retry: uint32(mills)})
 }
 
-func (s sse) send(buf *bytes.Buffer) (err error) {
-	_, err = buf.WriteTo(s[0])
+type flush interface {
+	FlushError() error
+}
+
+func (s *sse) Await(ctx context.Context) (err error) {
+	if s.ch == nil {
+		return io.EOF
+	}
+	defer func() {
+		close(s.ch)
+		s.ch = nil
+	}()
+	if ctx != nil {
+		return s.withContext(ctx)
+	} else {
+		return s.withoutContext()
+	}
+}
+func (s *sse) withoutContext() (err error) {
+	var buf = new(bytes.Buffer)
+	var ok bool
+	var m *msg
+	for {
+		select {
+		case m, ok = <-s.ch:
+			if !ok {
+				conf.Internal().Warn("sse chan closed")
+				return
+			}
+			err = s.write(m, buf)
+			if err != nil {
+				return
+			}
+			m = nil
+		}
+	}
+}
+func (s *sse) write(m *msg, buf *bytes.Buffer) (err error) {
+	if m.ping {
+		buf.Write(bPing)
+	} else if m.retry > 0 {
+		buf.Write(bRetry)
+		buf.WriteString(strconv.Itoa(int(m.retry)))
+		buf.Write(bLF)
+	} else {
+		if m.id != "" {
+			buf.Write(bId)
+			buf.WriteString(m.id)
+			buf.Write(bLF)
+		}
+		if m.event != "" {
+			buf.Write(bEvent)
+			buf.WriteString(m.event)
+			buf.Write(bLF)
+		}
+		if m.data != "" {
+			data := m.data
+			i := strings.IndexByte(data, '\n')
+			for i >= 0 {
+				buf.Write(bData)
+				buf.WriteString(data[:i])
+				buf.Write(bLF)
+				data = data[i+1:]
+				i = strings.IndexByte(data, '\n')
+			}
+			if len(data) > 0 {
+				buf.Write(bData)
+				buf.WriteString(data)
+				buf.Write(bLF)
+			}
+		}
+		buf.Write(bLF)
+	}
+	_, err = buf.WriteTo(s)
+	if err != nil {
+		return
+	}
+	err = s.ResponseWriter.(flush).FlushError()
+	if err != nil {
+		if _, ok := err.(*net.OpError); ok {
+			return io.EOF
+		}
+		return
+	}
+	buf.Reset()
 	return
 }
-func (s sse) Send(data string) (err error) {
+func (s *sse) withContext(ctx context.Context) (err error) {
+	var buf = new(bytes.Buffer)
+	var ok bool
+	var m *msg
+	for {
+		select {
+		case <-ctx.Done():
+			conf.Internal().Warn("sse context closed")
+			return
+		case m, ok = <-s.ch:
+			if !ok {
+				conf.Internal().Warn("sse chan closed")
+				return
+			}
+			err = s.write(m, buf)
+			if err != nil {
+				return
+			}
+			m = nil
+		}
+	}
+}
 
+func (s *sse) send(msg *msg) (err error) {
+	if s.ch == nil {
+		return io.EOF
+	}
+	log.Printf("Send %+v", msg)
+	s.ch <- msg
+	return
+}
+func (s *sse) Send(data string) (err error) {
 	return s.SendEventID("", "", data)
 }
 
-func (s sse) SendEvent(event, data string) (err error) {
+func (s *sse) SendEvent(event, data string) (err error) {
 	return s.SendEventID("", event, data)
 }
 
 // NewSSE create new SSE, should not send headers manually, this function will send Server-Send-Event headers.
-func NewSSE(w http.ResponseWriter) SSE {
+func NewSSE(w http.ResponseWriter, bufferSize ...int) (s SSE) {
 	w.Header().Add("Connection", "keep-alive")
 	w.Header().Add("Transfer-Encoding", "chunked")
 	w.Header().Add("Content-Type", "text/event-stream")
 	w.Header().Add("X-Accel-Buffering", "no")
 	w.Header().Add("Cache-Control", "no-cache, must-revalidate")
 	w.WriteHeader(200)
-	return &sse{w}
+	var buf = 1
+	if len(bufferSize) > 0 && bufferSize[0] > 0 {
+		buf = bufferSize[0]
+	}
+	s = &sse{make(chan *msg, buf), w}
+	_ = s.Ping()
+	return
 }
---
+++ htt/sse_test.go
@@ -0,0 +1,43 @@
+package htt
+
+import (
+	"errors"
+	"github.com/ZenLiuCN/fn"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestSSE(t *testing.T) {
+	fn.Panic(http.ListenAndServe(":8082", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Expose-Header", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		sse := NewSSE(w)
+		tick := time.NewTicker(time.Second)
+		go func(tk *time.Ticker) {
+			defer func() {
+				tk.Stop()
+			}()
+		loop:
+			for {
+				select {
+				case tm := <-tk.C:
+					t.Logf("on tick %s", tm)
+					err := sse.Send(tm.String())
+					if err != nil {
+						t.Logf("%s", err)
+						break loop
+					}
+				}
+			}
+			t.Logf("quit ticker")
+		}(tick)
+		err := sse.Await(nil)
+		if err != nil && !errors.Is(err, io.EOF) {
+			panic(err)
+		}
+		t.Logf("done")
+	})))
+}
